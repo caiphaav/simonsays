85bf044451ed79ebd3b24d0f22b3ad7a4bab3f80
Vlad G <vladislav.g@kpigroups.com>
2022-02-10 08:12:28 +0100

    feat(app): game config

--- app/shared/index.ts
+++ app/shared/index.ts
@@ -1,3 +1,4 @@
 export {useTheme, useThemedStyles, ThemeProvider, hdp, wdp} from './theme';
 export * as Types from './types';
 export {store, storeActions} from './store';
+export * as Constants from './constants';
